7f9bf964c52b617d75b4ab3ed917ddea42a67b7d
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-14 10:12:13 -0400

    Update 10-1Learning_Python.py

--- 10-1Learning_Python.py
+++ 10-1Learning_Python.py
@@ -16,3 +16,25 @@ lines = contents.splitlines()
 for line in lines:
     print(line)
 
+#Imporvixed improvement
+from pathlib import Path
+
+path = Path("python_sum.txt")
+contents = path.read_text()
+new_contents = path.read_text().replace('Python', 'C')
+contents = new_contents
+
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
+
+#Short version
+from pathlib import Path
+
+path = Path("python_sum.txt")
+new_contents = path.read_text().replace("Python", "Java")
+
+lines = new_contents.splitlines()
+for line in lines:
+    print(line)
