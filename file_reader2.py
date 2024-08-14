0e8b5cabb605fb0b5db9e278104a92c78f32962c
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-13 21:33:26 -0400

    Update file_reader2.py

--- file_reader2.py
+++ file_reader2.py
@@ -3,3 +3,12 @@ filename = 'pi_digits.txt'
 with open(filename) as file_object:
     for line in file_object:
         print(line.rstrip())
+
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
