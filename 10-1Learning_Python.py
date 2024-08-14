192f77ed3e6a3e864929b721240aa3c82ec68534
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-14 09:57:18 -0400

    Update 10-1Learning_Python.py

--- 10-1Learning_Python.py
+++ 10-1Learning_Python.py
@@ -1,1 +1,18 @@
+#Read file at once
+from pathlib import Path
+
+path = Path("python_sum.txt")
+contents = path.read_text()
+print(contents.rstrip())
+
+#Reading txt line by line 
+
+from pathlib import Path
+
+path = Path("python_sum.txt")
+contents = path.read_text()
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
 
