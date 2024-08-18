17c1f29030c6a8ecdcebe70f8f479371989b2d64
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-17 22:07:47 -0400

    Update write_message.py

--- write_message.py
+++ write_message.py
@@ -1,1 +1,15 @@
+from pathlib import Path
+
+path = Path("programming.txt")
+path.write_text("I love programming !") #This won't make any output but stores message into file
+
+#Writing multiple lines
+from pathlib import Path
+
+contents = "I love programming !\n"
+contents += "I love creating new games .\n"
+contents += "I also love working with data!"
+
+path = Path("programming.txt")
+path.write_text(contents)
 
