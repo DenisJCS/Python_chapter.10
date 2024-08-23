66caeffb712b56cf2ef4103f0448f32c8dcf0b65
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-22 23:06:26 -0400

    Update guest_book.py

--- guest_book.py
+++ guest_book.py
@@ -1,1 +1,16 @@
+from pathlib import Path 
+
+path = Path("guest_book.txt")
+
+print("Please enter the guest name .")
+print("Type 'q' when you are finished .")
+
+while True:
+    name = input("Enter your name: ")
+    if name == 'q':
+        break
+    else:
+        print(f"Hello, {name}. Welcome !")
+    with path.open(mode='a') as file:
+            file.write(f"{name}\n")
 
