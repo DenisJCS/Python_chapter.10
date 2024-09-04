10e9d8dd18eaef738013866fd583c1c04ed13538
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-03 21:02:51 -0400

    Update division_calculator.py

--- division_calculator.py
+++ division_calculator.py
@@ -19,37 +19,48 @@ while True:
     answer = int(first_number) / int(second_number)
     print(answer)
     
-denis_jcs@Deniss-MacBook-Air chapter_10 % python3 division_calculate.py
-You can't divide by zero
+#Here new version of program that take exception block
+try:
+    print(5/0)
+except: ZeroDivisionError
+print("You can't divide by zero")
+
+#Calculator does only division
+
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("\nSecond number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide bu 0!")
+    else:
+        print(answer)
+        
+# From terminal 
 Give me two numbers, and I'll divide them.
 Enter 'q' to quit.
 
-First number: 250
+First number: 7
 
-Second number: 15
-16.666666666666668
+Second number: 2
+3.5
 
-First number: 9999
+First number: 13
 
-Second number: 1717
-5.823529411764706
+Second number: 3
+4.333333333333333
 
-First number: q
-denis_jcs@Deniss-MacBook-Air chapter_10 % 
-
-#But this program does nothing to handle errors so asking it to divide by zero
-Give me two numbers, and I'll divide them.
-Enter 'q' to quit.
-
-First number: 10
-
-Second number: 50
-0.2
-
-First number: 100
+First number: 5
 
 Second number: 0
-Traceback (most recent call last):
-  File "/Users/denis_jcs/Desktop/chapter_10/division_calculate.py", line 18, in <module>
-    answer = int(first_number) / int(second_number)
-             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~
+You can't divide bu 0!
+
+
