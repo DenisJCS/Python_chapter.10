18d15bdb2eca18f0a5ec2096fc1283375e45f1ac
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-04 20:44:16 -0400

    Update word_count2.py

--- word_count2.py
+++ word_count2.py
@@ -1,1 +1,21 @@
+from pathlib import Path
+
+def count_words(filename):
+    """Count the approximate number of words in a file"""
+    try:
+
+        contents = filename.read_text(encoding = 'utf-8')
+
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
+    else:
+    #Count the approximate numberr of the words in the file:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
+filenames = ['alice.txt', 'siddhart.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    path = Path(filename)
+    count_words(path)
+
 
