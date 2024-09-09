29d54c94e2d9c465ed2e2ee2c80cd505cff48deb
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-08 21:35:15 -0400

    Update remember_me.py

--- remember_me.py
+++ remember_me.py
@@ -8,3 +8,22 @@ contents = json.dumps(username)
 path.write_text(contents)
 
 print(f"We'll remember you when come back, {username}")
+
+
+# Program below is combined storing and loading JSON and checks if user already stored any data and if it is , program open it up
+from pathlib import Path
+import json
+
+path = Path('username.json')
+
+if path.exists():
+    contents = path.read_text()
+    username = json.loads(contents)
+    print(f"Welcome back, {username}")
+else:
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    print(f"We will remember you when you come back, {username}")
+
+
