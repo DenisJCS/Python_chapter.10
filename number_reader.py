906ab61c193df6286f5ae8a1b707e63d849a61fb
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-09-08 21:12:41 -0400

    Update number_reader.py

--- number_reader.py
+++ number_reader.py
@@ -1,1 +1,9 @@
+from pathlib import Path
+import json
+
+path = Path('numbers.json')
+contents = path.read_text()
+numbers = json.loads(contents) # json.loads() imports data from json
+
+print(contents)
 
